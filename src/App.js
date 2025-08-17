9201db99ec97a49152942aadfe68f23308fed9e9
amanraooo <ak9070777@gmail.com>
2025-08-17 18:47:50 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Routes, Route} from 'react-router-dom';
 import Home from "./Pages/Home"
 function App() {
   return (
-    <div>
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Routes>
         <Route path="/" element={<Home/>}/>
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
       {/* Section 1 */}
+      <div className="relative mx-auto flex flex-col w-11/12 items-center text-white justify-between">
+        <Link to={"/signup"}>
+          <div className=" group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit">
+            <div className="flex flex-row items-center gap-3 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
+              <p>Become an Instructor</p>
+              <FaArrowRight />
+            </div>
+          </div>
+        </Link>
+      </div>
 
-      
       {/* Section 2 */}
 
-      
       {/* section 3 */}
 
-      
       {/* Footer */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
