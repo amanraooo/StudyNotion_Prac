1ac0d65dff32dc993ac1df13ef7ff7e52ca612b6
amanraooo <ak9070777@gmail.com>
2025-08-17 19:42:39 +0530

    Frontend in progress..

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import HighlightText from "../components/core/HomePage/HighlightText";
+
 const Home = () => {
   return (
     <div>
       {/* Section 1 */}
-      <div className="relative mx-auto flex flex-col w-11/12 items-center text-white justify-between">
+      <div className="relative mx-auto flex flex-col w-11/12 items-center text-white justify-between max-w-maxContent">
         <Link to={"/signup"}>
           <div className=" group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit">
             <div className="flex flex-row items-center gap-3 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
@@ -14,7 +16,21 @@ const Home = () => {
             </div>
           </div>
         </Link>
+
+        <div className="mt-7 text-center text-4xl font-semibold  ">
+            Empower Your Future With 
+            <HighlightText text={'Coding Skills'}/>
+        </div>
+
+        <div className="mt-4 w-[90%] text-center text-lg font-bold text-richblack-300">
+            With our online coding courses, you can learn at your own pace, from anywhere in the world, and get access to a wealth of resources, including hands-on projects, quizzes, and personalized feedback from instructors.
+        </div>
+
+
+
       </div>
+
+
 
       {/* Section 2 */}
 
