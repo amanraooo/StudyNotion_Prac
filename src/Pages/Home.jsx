1d88489ae147a63ba78bec5427ff19b792cb5123
amanraooo <ak9070777@gmail.com>
2025-08-17 18:06:09 +0530

    Frontend

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      {/* Section 1 */}
+
+      
+      {/* Section 2 */}
+
+      
+      {/* section 3 */}
+
+      
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
