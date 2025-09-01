12deae4b2f71b49514ea30e0e248bed65a0f8512
amanraooo <ak9070777@gmail.com>
2025-09-01 12:19:40 +0530

    InstructorSection

--- src/components/core/HomePage/InstructorSection.jsx
+++ src/components/core/HomePage/InstructorSection.jsx
@@ -1,40 +1,37 @@
 import React from "react";
 import Instructor from "../../../assets/Images/Instructor.png";
-import HighlightText from './HighlightText';
+import HighlightText from "./HighlightText";
 import CTAButton from "../../../components/core/HomePage/Button";
-import { FaArrowRight } from 'react-icons/fa';
-
+import { FaArrowRight } from "react-icons/fa";
 
 const InstructorSection = () => {
   return (
     <div className="mt-20">
-
       <div className="flex flex-row gap-20 items-center">
-
         <div className="w-[50%]">
           <img src={Instructor} alt="instructor" className="shadow-white" />
         </div>
 
         <div className="w-[50%] flex flex-col gap-10 ">
+          <div className="text-4xl font-semibold w-[50%]">
+            Become an
+            <HighlightText text={"Instructor"} />
+          </div>
 
-<div className='text-4xl font-semibold w-[50%]'>
-    Become an 
-    <HighlightText text={"Instructor"} />
-</div>
+          <p className="font-medium text-[16px] w-[90%] text-richblack-300">
+            Instructors from around the world teach millions of students on
+            StudyNotion. We provide the tools and skills to teach what you love.
+          </p>
 
-<p className="font-medium text-[16px] w-[90%] text-richblack-300">Instructors from around the world teach millions of students on StudyNotion. We provide the tools and skills to teach what you love.</p>
-
-<div className='w-fit'>
-<CTAButton active={true} linkto={"/signup"}>
-<div className="flex flex-row gap-2 w-fit items-center">
-    Start Learning Today 
-    <FaArrowRight/>
-</div>
-</CTAButton>
-</div>
-
+          <div className="w-fit">
+            <CTAButton active={true} linkto={"/signup"}>
+              <div className="flex flex-row gap-2 w-fit items-center">
+                Start Learning Today
+                <FaArrowRight />
+              </div>
+            </CTAButton>
+          </div>
         </div>
-
       </div>
     </div>
   );
