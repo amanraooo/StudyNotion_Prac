0d0630a5a269cedb21e1c0df3d4c0c4bbf3f46e3
amanraooo <ak9070777@gmail.com>
2025-09-05 18:14:06 +0530

    Logo added

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import logo from "../../assets/Logo/Logo-Full-Light.png"
+
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700">
+      <div className='flex w-11/12 max-w-maxContent items-center justify-between'>
+
+        <Link to='/'>
+        <img src={logo} alt="studynotionLogo"
+        width={160}
+        height={42}
+        loading='lazy' />
+        </Link>
+
+      </div>
     </div>
   )
 }
