e768da92949bfb867144479698be2f05881e9946
amanraooo <ak9070777@gmail.com>
2025-09-05 17:56:53 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from "./Pages/Home"
+import Navbar from './components/common/Navbar';
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
-    
+
   );
 }
 
