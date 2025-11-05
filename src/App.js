9e8f0a3d8fde511e777d3c592431e79dfbd9e8fd
amanraooo <ak9070777@gmail.com>
2025-11-05 15:32:01 +0530

    study notion

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./App.css";
 import { Routes, Route } from 'react-router-dom';
 import Home from "./Pages/Home"
 import Navbar from './components/common/Navbar';
+import Login from "./Pages/Login"
+import Signup from "./Pages/Signup"
+
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
       </Routes>
     </div>
 
