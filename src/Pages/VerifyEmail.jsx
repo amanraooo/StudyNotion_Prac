289cba42b766d0955a8ecadd0e6ee812dff530f8
amanraooo <ak9070777@gmail.com>
2025-11-06 22:59:44 +0530

    verify email

--- src/Pages/VerifyEmail.jsx
+++ src/Pages/VerifyEmail.jsx
@@ -2,41 +2,41 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 
 const VerifyEmail = () => {
-    const [otp,setOtp] = useState("");
+    const [otp, setOtp] = useState("");
     const dispatch = useDispatch();
-    const {loading} = useSelector((state)=>state.auth);
-    
-  return (
-    <div>
-      {
-        loading
-        ?
-        (
-            <div>
-                Loading...
-            </div>
-        ):(
-            <div>
-                <h1>Verify Email</h1>
-                <p>A verification code has been sent to you.Enter the code below</p>
+    const { loading } = useSelector((state) => state.auth);
 
-                <form>
-                    <OTPInput
-                    value={otp}
-                    onChange={setOtp}
-                    numInputs={6}
-                    renderInput={(props)=> <input {...props} />}
-                    />
+    return (
+        <div>
+            {
+                loading
+                    ?
+                    (
+                        <div>
+                            Loading...
+                        </div>
+                    ) : (
+                        <div>
+                            <h1>Verify Email</h1>
+                            <p>A verification code has been sent to you.Enter the code below</p>
 
-                    <button type='submit'>
-                        Verify Email
-                    </button>
-                </form>
-            </div>
-        )
-      }
-    </div>
-  )
+                            <form>
+                                <OTPInput
+                                    value={otp}
+                                    onChange={setOtp}
+                                    numInputs={6}
+                                    renderInput={(props) => <input {...props} />}
+                                />
+
+                                <button type='submit'>
+                                    Verify Email
+                                </button>
+                            </form>
+                        </div>
+                    )
+            }
+        </div>
+    )
 }
 
 export default VerifyEmail
