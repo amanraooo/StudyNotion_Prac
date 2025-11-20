934cb493937472f4bc0a1cf70a9ebae39bdbf486
amanraooo <ak9070777@gmail.com>
2025-11-20 19:54:40 +0530

    contact us form

--- src/components/common/ContactUsForm.jsx
+++ src/components/common/ContactUsForm.jsx
@@ -1,103 +1,161 @@
-import React, { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form';
+import React, { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
+
+import CountryCode from "../../data/countrycode.json"
+import { apiConnector } from "../../services/apiconnector"
+import { contactusEndpoint } from "../../services/apis"
 
 const ContactUsForm = () => {
+  const [loading, setLoading] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm()
 
-    const [loading, setLoading] = useState(false);
-    const { register,
-        handleSubmit,
-        reset,
-        formState: { errors, isSubmitSuccessful }
-    } = useForm();
-
-    const submitContactForm = async (data) => {
-
+  const submitContactForm = async (data) => {
+    // console.log("Form Data - ", data)
+    try {
+      setLoading(true)
+      const res = await apiConnector(
+        "POST",
+        contactusEndpoint.CONTACT_US_API,
+        data
+      )
+      // console.log("Email Res - ", res)
+      setLoading(false)
+    } catch (error) {
+      console.log("ERROR MESSAGE - ", error.message)
+      setLoading(false)
     }
-    useEffect(() => {
-        if (isSubmitSuccessful) {
-            reset({
-                email: "",
-                firstName: "",
-                lastname: "",
-                message: "",
-                phoneNo: "",
-            })
-        }
-    }, { reset, isSubmitSuccessful })
+  }
 
-    return (
-        <div onSubmit={handleSubmit(submitContactForm)}>
-            <div>
-                <div>
-                    <label htmlFor="firstname">First Name</label>
-                    <input
-                        type="text"
-                        name="firstname"
-                        id="firstname"
-                        placeholder='Enter first name'
-                        {...register("firstname", { required: true })}
-                    />
-                    {
-                        errors.firstname && (
-                            <span>
-                                Please enter your name
-                            </span>
-                        )
-                    }
-                </div>
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset({
+        email: "",
+        firstname: "",
+        lastname: "",
+        message: "",
+        phoneNo: "",
+      })
+    }
+  }, [reset, isSubmitSuccessful])
 
-                <div>
-                    <label htmlFor="lastname">Last Name</label>
-                    <input
-                        type="text"
-                        name="lastname"
-                        id="lastname"
-                        placeholder='Enter last name'
-                        {...register("lastname")}
-                    />
-
-                </div>
-
-                <div>
-                    <label htmlFor="email">Email Address</label>
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder='Enter email'
-                        {...register("email", { required: true })}
-                    />
-                    {
-                        errors.email && (
-                            <span>
-                                Please enter your Email
-                            </span>
-                        )
-                    }
-                </div>
-
-                <div>
-                    <label htmlFor="message">Message</label>
-                    <textarea
-                        name="message"
-                        id="message"
-                        placeholder='Enter message'
-                        cols="30"
-                        rows="7"
-                        {...register("message", { required: true })}
-                    />
-                    {
-                        errors.email && (
-                            <span>
-                                Please enter your Email
-                            </span>
-                        )
-                    }
-                </div>
-
-            </div>
+  return (
+    <form
+      className="flex flex-col gap-7"
+      onSubmit={handleSubmit(submitContactForm)}
+    >
+      <div className="flex flex-col gap-5 lg:flex-row">
+        <div className="flex flex-col gap-2 lg:w-[48%]">
+          <label htmlFor="firstname" className="lable-style">
+            First Name
+          </label>
+          <input
+            type="text"
+            name="firstname"
+            id="firstname"
+            placeholder="Enter first name"
+            className="form-style"
+            {...register("firstname", { required: true })}
+          />
+          {errors.firstname && (
+            <span className="-mt-1 text-[12px] text-yellow-100">
+              Please enter your name.
+            </span>
+          )}
         </div>
-    )
+        <div className="flex flex-col gap-2 lg:w-[48%]">
+          <label htmlFor="lastname" className="lable-style">
+            Last Name
+          </label>
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            placeholder="Enter last name"
+            className="form-style"
+            {...register("lastname")}
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="email" className="lable-style">
+          Email Address
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Enter email address"
+          className="form-style"
+          {...register("email", { required: true })}
+        />
+        {errors.email && (
+          <span className="-mt-1 text-[12px] text-yellow-100">
+            Please enter your Email address.
+          </span>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="phonenumber">Phone Number</label>
+
+        <div className="flex flex-row gap-5">
+        <div>
+            <select name="dropdown" id="dropdown"
+            {...register("countrycode", {required:true})}
+            >
+{
+    CountryCode.map((ele,index)=>{
+        return (
+            <option>
+                {ele.code} - {ele.country}
+            </option>
+        )
+    })
+}
+            </select>
+        </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="message" className="lable-style">
+          Message
+        </label>
+        <textarea
+          name="message"
+          id="message"
+          cols="30"
+          rows="7"
+          placeholder="Enter your message here"
+          className="form-style"
+          {...register("message", { required: true })}
+        />
+        {errors.message && (
+          <span className="-mt-1 text-[12px] text-yellow-100">
+            Please enter your Message.
+          </span>
+        )}
+      </div>
+
+      <button
+        disabled={loading}
+        type="submit"
+        className={`rounded-md bg-yellow-50 px-6 py-3 text-center text-[13px] font-bold text-black shadow-[2px_2px_0px_0px_rgba(255,255,255,0.18)] 
+         ${
+           !loading &&
+           "transition-all duration-200 hover:scale-95 hover:shadow-none"
+         }  disabled:bg-richblack-500 sm:text-[16px] `}
+      >
+        Send Message
+      </button>
+    </form>
+  )
 }
 
 export default ContactUsForm
