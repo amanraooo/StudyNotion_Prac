ff321b6368519084f68809bc1afd0a8eb014ad07
amanraooo <ak9070777@gmail.com>
2025-11-22 17:16:53 +0530

    sidebar

--- src/components/core/Dashboard/Sidebar.jsx
+++ src/components/core/Dashboard/Sidebar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { sidebarLinks } from './../../../data/dashboard-links';
+import { logout } from '../../../services/operations/authAPI';
 
 const Sidebar = () => {
+
+    const {loading: authLoading}= useSelector((state)=> state.auth);
+    const {user, loading : profileLoading}= useSelector((state)=> state.profile);
+
+    if(profileLoading || authLoading){
+        return (
+            <div className='mt-10'>
+                Loading...
+            </div>
+        )
+    }
   return (
     <div>
-      
+      <div>
+        
+      </div>
     </div>
   )
 }
