367ed595cc179ac10176dab46ed9f97da462a56c
amanraooo <ak9070777@gmail.com>
2025-11-23 21:31:23 +0530

    sidebar

--- src/components/core/Dashboard/Sidebar.jsx
+++ src/components/core/Dashboard/Sidebar.jsx
@@ -6,30 +6,30 @@ import SidebarLink from './SidebarLink';
 
 const Sidebar = () => {
 
-    const {loading: authLoading}= useSelector((state)=> state.auth);
-    const {user, loading : profileLoading}= useSelector((state)=> state.profile);
+  const { loading: authLoading } = useSelector((state) => state.auth);
+  const { user, loading: profileLoading } = useSelector((state) => state.profile);
 
-    if(profileLoading || authLoading){
-        return (
-            <div className='mt-10'>
-                Loading...
-            </div>
-        )
-    }
+  if (profileLoading || authLoading) {
+    return (
+      <div className='mt-10'>
+        Loading...
+      </div>
+    )
+  }
   return (
     <div>
       <div className="flex min-w-[222px] flex-col border-r-[1px] border-r-richblack-700 h-[calc[100vh-3.5rem]] bg-richblack-800 py-10">
-    <div className='flex flex-col'>
-    {
-        sidebarLinks.map((link,index)=>{
-            if(link.type && user?.accountType !== link.type) return null;
-            return (
+        <div className='flex flex-col'>
+          {
+            sidebarLinks.map((link, index) => {
+              if (link.type && user?.accountType !== link.type) return null;
+              return (
                 <SidebarLink key={link.id} link={link} iconName={link.icon} />
-            )
+              )
 
-        })
-    }
-    </div>
+            })
+          }
+        </div>
       </div>
     </div>
   )
