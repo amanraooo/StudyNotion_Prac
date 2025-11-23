38f28d563c129a89336ae6e719325418c8cb9e68
amanraooo <ak9070777@gmail.com>
2025-11-23 21:24:32 +0530

    sidebarlink

--- src/components/core/Dashboard/SidebarLink.jsx
+++ src/components/core/Dashboard/SidebarLink.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import {useLocation, NavLink} from 'react-router-dom'
 
-const SidebarLink = () => {
+const SidebarLink = ({link,iconName}) => {
+
+  const Icon = Icons[iconName];
+  const  location = useLocation();
+  const dispatch = useDispatch();
+
+  const matchRoute = (route)=>{
+    return matchRoute({path:route}, location.pathname);
+  }
   return (
-    <div>
-      
-    </div>
+    <NavLink
+    to={link.path}
+    className={`relative px-8 py-2 text-sm font-medium ${matchRoute(link.path) ? "bg=yellow-800": "bg-opacity-0"}`}
+    >
+      <span className={`absolute left-0 top-0 h-full w-[0.2rem] bg-yellow-50 ${matchRoute(link.path) ? "opacity-100": "opacity-0"}`}>
+
+<div className='fx items-center gap-x-2'>
+  <Icon className="text-lg"/>
+  <span>{link.name}</span>
+</div>
+      </span>
+    </NavLink>
   )
 }
 
