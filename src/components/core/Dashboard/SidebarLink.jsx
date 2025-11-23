c027e57bfb1a04b7646d9ed5060d5d8965897b05
amanraooo <ak9070777@gmail.com>
2025-11-23 21:24:58 +0530

    sidebarlink

--- src/components/core/Dashboard/SidebarLink.jsx
+++ src/components/core/Dashboard/SidebarLink.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
-import {useLocation, NavLink} from 'react-router-dom'
+import { useLocation, NavLink } from 'react-router-dom'
 
-const SidebarLink = ({link,iconName}) => {
+const SidebarLink = ({ link, iconName }) => {
 
   const Icon = Icons[iconName];
-  const  location = useLocation();
+  const location = useLocation();
   const dispatch = useDispatch();
 
-  const matchRoute = (route)=>{
-    return matchRoute({path:route}, location.pathname);
+  const matchRoute = (route) => {
+    return matchRoute({ path: route }, location.pathname);
   }
   return (
     <NavLink
-    to={link.path}
-    className={`relative px-8 py-2 text-sm font-medium ${matchRoute(link.path) ? "bg=yellow-800": "bg-opacity-0"}`}
+      to={link.path}
+      className={`relative px-8 py-2 text-sm font-medium ${matchRoute(link.path) ? "bg=yellow-800" : "bg-opacity-0"}`}
     >
-      <span className={`absolute left-0 top-0 h-full w-[0.2rem] bg-yellow-50 ${matchRoute(link.path) ? "opacity-100": "opacity-0"}`}>
+      <span className={`absolute left-0 top-0 h-full w-[0.2rem] bg-yellow-50 ${matchRoute(link.path) ? "opacity-100" : "opacity-0"}`}>
 
-<div className='fx items-center gap-x-2'>
-  <Icon className="text-lg"/>
-  <span>{link.name}</span>
-</div>
+        <div className='fx items-center gap-x-2'>
+          <Icon className="text-lg" />
+          <span>{link.name}</span>
+        </div>
       </span>
     </NavLink>
   )
