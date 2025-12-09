fcba57cad2bccd3d9b33487ed20e8ff6113d1ada
amanraooo <ak9070777@gmail.com>
2025-12-09 16:49:09 +0530

    My Profile slice

--- src/slices/profileSlice.js
+++ src/slices/profileSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    user: null,
+    user: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : null,
     loading: false
 };
 
