b267b57ba709a4b90d3f42dfab7f762a56b11f56
amanraooo <ak9070777@gmail.com>
2025-12-11 15:57:39 +0530

    sidebarlink.jsx

--- src/components/core/Dashboard/SidebarLink.jsx
+++ src/components/core/Dashboard/SidebarLink.jsx
@@ -9,14 +9,14 @@ const SidebarLink = ({ link, iconName }) => {
   const dispatch = useDispatch();
 
   const matchRoute = (route) => {
-    return matchRoute({ path: route }, location.pathname);
+    return matchPath({ path: route }, location.pathname);
   }
   return (
     <NavLink
       to={link.path}
       className={`relative px-8 py-2 text-sm font-medium ${matchRoute(link.path) ? "bg=yellow-800" : "bg-opacity-0"}`}
     >
-      <span className={`absolute left-0 top-0 h-full w-[0.2rem] bg-yellow-50 ${matchRoute(link.path) ? "opacity-100" : "opacity-0"}`}>
+      <span className={`absolute left-0 top-0 h-full w-[0.2rem] bg-yellow-50 ${matchPath(link.path) ? "opacity-100" : "opacity-0"}`}>
 
         <div className='fx items-center gap-x-2'>
           <Icon className="text-lg" />
