25e96b00ddd5e1b2a222eded08661b56c4d0e499
amanraooo <ak9070777@gmail.com>
2025-12-17 00:41:52 +0530

    cart

---
+++ src/components/core/Dashboard/Cart/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const cart = () => {
+
+    const { total, totalItems } = useSelector((state) => state.auth);
+
+    return (
+        <div>
+            <h1>My Cart</h1>
+            <p>{totalItems} Courses in Cart</p>
+
+            {
+                total > 0
+                    ?
+                    (<div>
+                        <RenderCartCourses />
+                        <RenderTotalAmount />
+                    </div>)
+                    :
+                    (<p>Your Cart is Empty</p>)
+            }
+        </div>
+    )
+}
+
+export default cart
