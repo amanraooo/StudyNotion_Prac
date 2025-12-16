729da614c3350f393a1784668d553e873dc8e3d1
amanraooo <ak9070777@gmail.com>
2025-12-17 00:12:07 +0530

    enrolled courses

--- src/components/core/Dashboard/EnrolledCourses.jsx
+++ src/components/core/Dashboard/EnrolledCourses.jsx
@@ -23,7 +23,56 @@ const EnrolledCourses = () => {
 
     return (
         <div>
+            <div>
+               Enrolled Courses
+            </div>
 
+            {
+                !enrolledCourses ?
+                 (
+                 <div>Loading... </div>
+                ):
+                 
+                    !enrolledCourses.length ? (<p>You have not enrolled in any courses</p>)
+                 :(
+                    <div>
+                        <div>
+                            <p>Course Name</p>
+                            <p>Duration</p>
+                            <p>Progress</p>
+                        </div>
+
+                        {/* cards */}
+                        {
+                            enrolledCourses.map((course,index)=>{
+                                <div>
+                                    <div>
+                                        <img src={course.thumbnail}/>
+                                        <div>
+                                            <p>{course.courseName}</p>
+                                            <p>
+                                       {course.courseDescription}         
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        {course?.totalDuration}
+                                    </div>
+
+                                <div>
+                                    <p>Progress : {course.progressPercentage || 0}%</p>
+                                    <ProgressBar 
+                                    completed={course.progressPercentage || 0}
+height='8px'                                    isLabelVisible={false}
+                                    />
+                                </div>
+                                </div>
+                            })
+                        }
+                    </div>
+                 )
+            }
         </div>
     )
 }
