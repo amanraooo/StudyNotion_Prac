b4b43d79f6c2d9379edda01abd1b4aac0d44571e
amanraooo <ak9070777@gmail.com>
2025-12-17 01:03:11 +0530

    cart

--- src/components/core/Dashboard/Cart/RenderCartCourses.jsx
+++ src/components/core/Dashboard/Cart/RenderCartCourses.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
 const RenderCartCourses = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            {
+                cart.map((course, index) => {
+                    <div>
+                        <div>
+                            <img src={course?.thumbnail} />
+
+                            <div>
+                                <p>{course?.courseName}</p>
+                                <p>{course?.category?.name}</p>
+                                <div>
+                                    <span>4.8</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                })
+            }
+        </div>
+    )
 }
 
 export default RenderCartCourses
