92276e1c82fdf56c917a2da5203b6d12a50e8f9e
amanraooo <ak9070777@gmail.com>
2025-12-17 00:12:46 +0530

    enrolled courses

--- src/components/core/Dashboard/EnrolledCourses.jsx
+++ src/components/core/Dashboard/EnrolledCourses.jsx
@@ -24,54 +24,54 @@ const EnrolledCourses = () => {
     return (
         <div>
             <div>
-               Enrolled Courses
+                Enrolled Courses
             </div>
 
             {
                 !enrolledCourses ?
-                 (
-                 <div>Loading... </div>
-                ):
-                 
+                    (
+                        <div>Loading... </div>
+                    ) :
+
                     !enrolledCourses.length ? (<p>You have not enrolled in any courses</p>)
-                 :(
-                    <div>
-                        <div>
-                            <p>Course Name</p>
-                            <p>Duration</p>
-                            <p>Progress</p>
-                        </div>
-
-                        {/* cards */}
-                        {
-                            enrolledCourses.map((course,index)=>{
+                        : (
+                            <div>
                                 <div>
-                                    <div>
-                                        <img src={course.thumbnail}/>
+                                    <p>Course Name</p>
+                                    <p>Duration</p>
+                                    <p>Progress</p>
+                                </div>
+
+                                {/* cards */}
+                                {
+                                    enrolledCourses.map((course, index) => {
                                         <div>
-                                            <p>{course.courseName}</p>
-                                            <p>
-                                       {course.courseDescription}         
-                                            </p>
+                                            <div>
+                                                <img src={course.thumbnail} />
+                                                <div>
+                                                    <p>{course.courseName}</p>
+                                                    <p>
+                                                        {course.courseDescription}
+                                                    </p>
+                                                </div>
+                                            </div>
+
+                                            <div>
+                                                {course?.totalDuration}
+                                            </div>
+
+                                            <div>
+                                                <p>Progress : {course.progressPercentage || 0}%</p>
+                                                <ProgressBar
+                                                    completed={course.progressPercentage || 0}
+                                                    height='8px' isLabelVisible={false}
+                                                />
+                                            </div>
                                         </div>
-                                    </div>
-
-                                    <div>
-                                        {course?.totalDuration}
-                                    </div>
-
-                                <div>
-                                    <p>Progress : {course.progressPercentage || 0}%</p>
-                                    <ProgressBar 
-                                    completed={course.progressPercentage || 0}
-height='8px'                                    isLabelVisible={false}
-                                    />
-                                </div>
-                                </div>
-                            })
-                        }
-                    </div>
-                 )
+                                    })
+                                }
+                            </div>
+                        )
             }
         </div>
     )
