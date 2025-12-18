5eef0d1fef7d0c4ead5d2a8ffb262195cef29a8c
amanraooo <ak9070777@gmail.com>
2025-12-18 12:42:28 +0530

    render total amount

--- src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import IconBtn from './../../../common/IconBtn';
+import { useSelector } from 'react-redux';
 
 const RenderTotalAmount = () => {
+
+  const {total} =useSelector((state)=> state.cart)
+
+  const handleBuyCourse =()=>{
+    const courses = cart.map((course)=> course.id);
+    console.log("bought these courses", courses);
+  }
   return (
     <div>
-      
+      <p>Total</p>
+      <p>Rs {total}</p>
+
+      <IconBtn
+      text="Buy Now"
+      onclick={handleBuyCourse}
+      customClasses={"w-full justify-center"}
+      />
     </div>
   )
 }
