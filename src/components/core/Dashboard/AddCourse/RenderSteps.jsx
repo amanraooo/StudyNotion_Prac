5602d539cc7717035219e0ae3fd91b5d25deadd4
amanraooo <ak9070777@gmail.com>
2025-12-20 16:40:09 +0530

    add course

--- src/components/core/Dashboard/AddCourse/RenderSteps.jsx
+++ src/components/core/Dashboard/AddCourse/RenderSteps.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 
 const RenderSteps = () => {
+
+  const steps = [
+    {
+      id: 1,
+      title: "Course Information",
+    },
+    {
+      id: 2,
+      title: "Course Builder",
+    },
+    {
+      id: 3,
+      title: "Publish",
+    }
+  ]
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        {
+          steps.map((item) => {
+            <>
+              <div className={`${step === item.id
+                ? "bg-yellow-900 border-yellow-50 text-yellow-50" : "border-richblack-700 bg-richblack-800 text-richblack-50"
+                }`}>
+
+              </div>
+            </>
+          })
+        }
+      </div>
+    </>
+
   )
 }
 
