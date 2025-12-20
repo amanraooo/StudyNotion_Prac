c2a6d6715064cf9bfb644e8a51417024fb12f5cd
amanraooo <ak9070777@gmail.com>
2025-12-20 20:15:23 +0530

    add course

--- src/components/core/Dashboard/AddCourse/CourseInformationForm/CourseInformationForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseInformationForm/CourseInformationForm.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { useDispatch, useSelector } from 'react-redux';
 
 const CourseInformationForm = () => {
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    getvalue,
+    formState: { error },
+  } = useForm();
+
+  const dispatch = useDispatch();
+  const { course, editCourse } = useSelector((state) => state.course);
+  const [loading, setLoading] = useState(false);
+  const [courseCategories, setCourseCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      setLoading(true);
+      const categories = await fetchCourseCategories();
+      if (categories.length > 0) {
+        setCourseCategories(categories);
+      }
+      setLoading(false);
+    }
+  })
   return (
     <div>
-      
+
     </div>
   )
 }
