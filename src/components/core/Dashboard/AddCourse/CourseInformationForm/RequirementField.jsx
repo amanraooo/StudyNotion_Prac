411a8522a6719939ba7bca56318ec60227b7f34f
amanraooo <ak9070777@gmail.com>
2025-12-23 20:18:40 +0530

    add course

---
+++ src/components/core/Dashboard/AddCourse/CourseInformationForm/RequirementField.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const RequirementField = ({ name, label, register, errors, setValue, getValues }) => {
+
+    const [requirement, setRequirement] = useState("");
+    const [requirementList, setRequirementList] = useState([]);
+
+    const handleAddRequirement = () => {
+        if (requirement) {
+            setRequirementList([...requirementList, requirement]);
+            setRequirement("");
+        }
+    }
+
+    const handleRemoveRequirement = () => {
+        if (requirement) {
+            setRequirementList([...requirementList, requirement]);
+            setRequirement("");
+        }
+    }
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default RequirementField
